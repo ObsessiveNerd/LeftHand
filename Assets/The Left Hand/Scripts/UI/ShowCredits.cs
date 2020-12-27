793b96c65c18f817dbcf204b78b97364834b530d
David Latham <dlatham@live.com>
2020-12-27 00:59:04 -0600

    Credits added, ver 0.1.1

---
+++ Assets/The Left Hand/Scripts/UI/ShowCredits.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCredits : Escape
+{
+    public GameObject Credits;
+
+    public void DisplayCredits()
+    {
+        Credits.SetActive(true);
+    }
+
+    protected override void Cleanup()
+    {
+        Credits.SetActive(false);
+    }
+}
